c078a51ed9f906b5fde4351aae599a2cff31c31c
Hidetake Iwata <int128@gmail.com>
2025-11-23 11:15:23 +0900

    Migrate to @octokit/action

---
+++ src/github.ts
@@ -0,0 +1,31 @@
+import assert from 'node:assert'
+import { Octokit } from '@octokit/action'
+import { retry } from '@octokit/plugin-retry'
+
+export const getOctokit = () => new (Octokit.plugin(retry))()
+
+export type Context = {
+  repo: {
+    owner: string
+    repo: string
+  }
+  workflow: string
+}
+
+export const getContext = async (): Promise<Context> => {
+  // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+  return {
+    repo: getRepo(),
+    workflow: getEnv('GITHUB_WORKFLOW'),
+  }
+}
+
+const getRepo = () => {
+  const [owner, repo] = getEnv('GITHUB_REPOSITORY').split('/')
+  return { owner, repo }
+}
+
+const getEnv = (name: string): string => {
+  assert(process.env[name], `${name} is required`)
+  return process.env[name]
+}
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as core from '@actions/core'
+import * as github from './github.js'
+import { run } from './run.js'
+
+const main = async (): Promise<void> => {
+  await run(
+    {
+      datadogAPIKey: core.getInput('datadog-api-key') || undefined,
+      datadogTags: core.getMultilineInput('datadog-tags'),
+    },
+    github.getOctokit(),
+    await github.getContext(),
+  )
+}
+
+try {
+  await main()
+} catch (e) {
+  core.setFailed(e instanceof Error ? e : String(e))
+  console.error(e)
+}
